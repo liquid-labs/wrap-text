223aa5ba09b6880fb1ee490708bb02e6b36bf2e4
Zane Rockenbaugh <zane@liquid-labs.com>
2023-12-22 11:58:46 -0600

    Support all options on CLI; update README.md

--- src/cli/wrap.mjs
+++ src/cli/wrap.mjs
@@ -1,6 +1,7 @@
 import * as fs from 'node:fs/promises'
 
 import commandLineArgs from 'command-line-args'
+import { commandLineDocumentation } from 'command-line-documentation'
 
 import { wrap as doWrap } from '../lib/wrap'
 
@@ -8,14 +9,22 @@ const cliSpec = {
   mainCommand: 'wrap',
   mainOptions: [
     { name: 'input-file', defaultOption: true, description: 'The file to process and wrap.' },
-    { name: 'hanging-indent', description: 'Adds a hanging indent of the specified width.', type: Number },
-    { name: 'indent', description: 'Indents the wrapped text the specified width.', type: Number },
+    { name: 'document', description: 'Creates documentation for self.', type: Boolean },
+    { 
+      name: 'document-section-depth', 
+      description: 'Sets the initial section-depth for generated docuemnation.', 
+      type: Number
+    },
+    { name: 'document-title', description: 'Sets the title for generated documentation.' },
+    { name: 'hanging-indent', description: 'The amount to indent all but the first line of a paragraph. Incompatible with other indent modes.', type: Number },
+    { name: 'ignore-tags', description: "Treat any tags ('<...>') in the text as 'invisible' and adjust wrapping accordingly."},
+    { name: 'indent', description: 'Indent each line by the spcified amount. Incompatible with other indent modes.', type: Number },
     { 
       name: 'prefix', 
       description: "Prefixes each wrapped line with the indicated prefix. Note this happens after the lines are wrapped according to the specified width. If you need the line to be a specific width in total, you must subtract the length of the indent yourself."
     },
     { name: 'smart-indent', description: 'Ignores tags (treats as zero-width string) when wrapping.', type: Boolean },
-    { name: 'width', description: 'The character width to wrap.', type: Number }
+    { name: 'width', description: 'The width to wrap to. Defaults to 80.', type: Number }
   ]
 }
 
@@ -29,10 +38,21 @@ const wrap = async ({
 
   const inputFile = options['input-file']
   const hangingIndent = options['hanging-indent']
+  const ignoreTags = options['ignore-tags']
   const smartIndent = options['smart-indent']
-  const { indent, prefix, width } = options
+  const { document: doDocument, indent, prefix, width } = options
 
-  const wrapOptions = { hangingIndent, indent, prefix, smartIndent, width }
+  if (doDocument === true) {
+    const sectionDepth = options['document-section-depth']
+    const title = options['document-title']
+
+    const documentation = commandLineDocumentation(cliSpec, { sectionDepth, title })
+    stdout.write(documentation)
+
+    return 0
+  }
+
+  const wrapOptions = { hangingIndent, ignoreTags, indent, prefix, smartIndent, width }
 
   if (inputFile !== undefined) {
     try {
